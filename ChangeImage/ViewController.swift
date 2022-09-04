71f5ae418090a8cfa50693e99a39ba67f524d889
Alexander Karpinets <alexanderkarpinets@gmail.com>
2022-09-04 21:50:01 +0300

    Done

--- ChangeImage/ViewController.swift
+++ ChangeImage/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let squared = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        squared.backgroundColor = .green
+        view.addSubview(squared)
     }
-
-
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+        let image = UIImage(named: "nature")
+        imageView.image = image
+        
+    }
+    @IBAction func startTimerButton(_ sender: UIButton) {
+        getTimer()
+    }
+    
+    @IBAction func endTimerButton(_ sender: UIButton) {
+        stopTimer()
+    }
+    
+    
+    func getMove() {
+        squared.frame.origin.x += 100
+        UIView.animate(withDuration: 0.3) {
+            self.squared.frame.origin.x -= 100
+            
+        }
+    }
+    func getTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            self.getMove()
+        })
+        timer.fire()
+    }
+    
+    func stopTimer() {
+        timer.invalidate()
+    }
+    
 }
+
 
